2199695d20a4b4afe2c14327b52ca980754dafb6
Hiba El otmani <elotmani@et.esiea.fr>
2026-03-25 13:01:13 +0100

    fic bug test7 and 8

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", function(req, res) {
+  let now = new Date();
+  res.json({ unix: now.getTime(), utc: now.toUTCString() });
+});
+
 app.get("/api/:date?", function(req, res) {
   let input = req.params.date;
 
